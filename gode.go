71ca43824420b26f2d67aa38fbf961657cdf59bc
Anton Moscal <msk@kouzdra.ru>
2017-01-15 11:51:38 +0300

    styled test

--- gode.go
+++ gode.go
@@ -2,38 +2,72 @@ package main
 
 import (
 	"log"
+	_ "github.com/mattn/go-gtk/gdkpixbuf"
+	_ "github.com/mattn/go-gtk/glib"
 	"github.com/mattn/go-gtk/gtk"
-	gsci "github.com/kouzdra/go-scintilla/gtkscintilla"
+	gsci "github.com/kouzdra/go-scintilla/gtk"
 	"os"
+	"fmt"
 )
 
 var _ = log.Printf
 
-func main() {
-	gtk.Init(&os.Args)
-	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
-	window.SetTitle("Editor")
-	window.Connect("destroy", gtk.MainQuit)
+type IDE struct {
+	window  *gtk .Window
+	sci     *gsci.Scintilla
+	menubar *gtk .MenuBar
+	RED      gsci.Style
+}
+
+func NewIDE () *IDE {
+	ide := &IDE{}
+	
+	ide.window = gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	ide.window.SetTitle("Editor")
+	ide.window.Connect("destroy", gtk.MainQuit)
+
+	vbox := gtk.NewVBox(false, 1)
+	ide.sci = gsci.NewScintilla()
+	ide.MakeMenu()
+	vbox.PackStart(ide.menubar, false, false, 0)
 
 	swin := gtk.NewScrolledWindow(nil, nil)
 	swin.SetPolicy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 	swin.SetShadowType(gtk.SHADOW_IN)
+	vbox.Add(swin)
 
-	sci := gsci.NewScintilla()
-	swin.Add(sci)
+	swin.Add(ide.sci)
 
-	window.Add(swin)
-	window.SetSizeRequest(400, 300)
-	window.ShowAll()
+	ide.window.Add(vbox)
+	ide.window.SetSizeRequest(400, 300)
+	ide.window.ShowAll()
+
+	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	window.SetTitle("Editor")
+	window.Connect("destroy", gtk.MainQuit)
+
+	return ide
+}
 
 
-	RED := uint (1)
-	sci.StyleResetDefault()
-	sci.StyleSetFg (RED, 0x0000FF);
-	//sci.StyleSetBg (RED, 0x808080);
-	sci.StyleSetUnderline (RED, false);
+func main() {
+	gtk.Init(&os.Args)
+	ide := NewIDE ()
+	ide.PreloadTest ()
+	gtk.Main()
+}
 
-	sci.SetText(`#include <iostream>
+func (ide *IDE) PreloadTest () {
+	ide.RED = gsci.Style (1)
+
+	s := ide.sci.Styling
+	s.ResetDefault()
+	s.SetFg (ide.RED, gsci.Color (0x0000FF));
+	//s.SetBg (ide.RED, gsci.``Color (0x808080));
+	s.SetUnderline (ide.RED, false);
+	//s.SetFont (ide.RED, "Sans Bold Italic 10")
+
+	ide.sci.SetText(`#include <iostream>
 template<class T>
 struct foo_base {
   T operator+(T const &rhs) const {
@@ -43,13 +77,77 @@ struct foo_base {
   }
 };
 `)
-	sci.GetStyleAt (5)
-	sci.StartStyling (3)
-	sci.SetStyling (10, RED)
-	log.Printf ("AT=%d\n", sci.GetStyleAt (5))
-	log.Printf ("AT=%d\n", sci.GetCharAt (5))
-	sci.GetEndStyled ()
+	s.GetAt (5)
+	s.Start (3)
+	s.Set (10, ide.RED)
+	log.Printf ("AT=%d\n", s.GetAt (5))
+	log.Printf ("AT=%d\n", ide.sci.GetCharAt (5))
+	s.GetEnd ()
 
+}
 
-	gtk.Main()
+func (ide *IDE) MakeMenu () {
+	//--------------------------------------------------------
+	// GtkMenuItem
+	//--------------------------------------------------------
+	ide.menubar = gtk.NewMenuBar ()
+	
+	{
+		cascademenu := gtk.NewMenuItemWithMnemonic("_File")
+		ide.menubar.Append(cascademenu)
+		submenu := gtk.NewMenu()
+		cascademenu.SetSubmenu(submenu)
+		
+		//var menuitem *gtk.MenuItem
+		menuitem := gtk.NewMenuItemWithMnemonic("E_xit")
+		menuitem.Connect("activate", func() {
+			gtk.MainQuit()
+		})
+		submenu.Append(menuitem)
+	}
+
+	{
+		cascademenu := gtk.NewMenuItemWithMnemonic("_View")
+		ide.menubar.Append(cascademenu)
+		submenu := gtk.NewMenu()
+		cascademenu.SetSubmenu(submenu)
+
+		menuitem := gtk.NewMenuItemWithMnemonic("_Font")
+		menuitem.Connect("activate", func() {
+			fsd := gtk.NewFontSelectionDialog("Font")
+			fsd.Response(func() {
+				fmt.Println(fsd.GetFontName())
+				ide.sci.Styling.SetFont(ide.RED, fsd.GetFontName())
+				ide.sci.Styling.SetUnderline (ide.RED, true);
+				fsd.Destroy()
+			})
+			fsd.SetTransientFor(ide.window)
+			fsd.Run()
+		})
+		submenu.Append(menuitem)
+	}
+
+	{
+		cascademenu := gtk.NewMenuItemWithMnemonic("_Help")
+		ide.menubar.Append(cascademenu)
+		submenu := gtk.NewMenu()
+		cascademenu.SetSubmenu(submenu)
+
+		menuitem := gtk.NewMenuItemWithMnemonic("_About")
+		menuitem.Connect("activate", func() {
+			dialog := gtk.NewAboutDialog()
+			dialog.SetName("Go-Gtk Demo!")
+			dialog.SetProgramName("demo")
+			dialog.SetAuthors([]string{"Kouzdra"})
+			//dir, _ := filepath.Split(os.Args[0])
+			//imagefile := filepath.Join(dir, "../../data/mattn-logo.png")
+			//pixbuf, _ := gdkpixbuf.NewPixbufFromFile(imagefile)
+			//dialog.SetLogo(pixbuf)
+			dialog.SetLicense("The library is available under the same terms and conditions as the Go, the BSD style license, and the LGPL (Lesser GNU Public License). The idea is that if you can use Go (and Gtk) in a project, you should also be able to use go-gtk.")
+			dialog.SetWrapLicense(true)
+			dialog.Run()
+			dialog.Destroy()
+		})
+		submenu.Append(menuitem)
+	}
 }
