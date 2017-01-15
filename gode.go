5505baa8b2b67aead488c254ef003fbafbddf838
Anton Moscal <msk@kouzdra.ru>
2017-01-15 07:51:12 +0300

    new go-sci

--- gode.go
+++ gode.go
@@ -1,12 +1,13 @@
 package main
 
-//# cgo LDFLAGS='-lgmodule-2.0 --static'
-
 import (
+	"log"
 	"github.com/mattn/go-gtk/gtk"
 	gsci "github.com/kouzdra/go-scintilla/gtkscintilla"
 	"os"
 )
+
+var _ = log.Printf
 
 func main() {
 	gtk.Init(&os.Args)
@@ -17,8 +18,22 @@ func main() {
 	swin := gtk.NewScrolledWindow(nil, nil)
 	swin.SetPolicy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 	swin.SetShadowType(gtk.SHADOW_IN)
-	sourceview := gsci.NewScintilla()
-	sourceview.InsertText(0, `#include <iostream>
+
+	sci := gsci.NewScintilla()
+	swin.Add(sci)
+
+	window.Add(swin)
+	window.SetSizeRequest(400, 300)
+	window.ShowAll()
+
+
+	RED := uint (1)
+	sci.StyleResetDefault()
+	sci.StyleSetFg (RED, 0x0000FF);
+	//sci.StyleSetBg (RED, 0x808080);
+	sci.StyleSetUnderline (RED, false);
+
+	sci.SetText(`#include <iostream>
 template<class T>
 struct foo_base {
   T operator+(T const &rhs) const {
@@ -28,12 +43,13 @@ struct foo_base {
   }
 };
 `)
+	sci.GetStyleAt (5)
+	sci.StartStyling (3)
+	sci.SetStyling (10, RED)
+	log.Printf ("AT=%d\n", sci.GetStyleAt (5))
+	log.Printf ("AT=%d\n", sci.GetCharAt (5))
+	sci.GetEndStyled ()
 
-	swin.Add(sourceview)
-
-	window.Add(swin)
-	window.SetSizeRequest(400, 300)
-	window.ShowAll()
 
 	gtk.Main()
 }
